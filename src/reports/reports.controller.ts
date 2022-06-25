02d6fb782d21ba005e30b954265e6dd1558344d8
Bora Özışık <ozisikbora@gmail.com>
2022-06-26 00:59:25 +0300

    application completed

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -24,7 +24,7 @@ export class ReportsController {
 
   @Get()
   getEstimate(@Query() query: GetEstimateDto) {
-    console.log(query);
+    return this.reportsService.createEstimate(query);
   }
 
   @Post()
